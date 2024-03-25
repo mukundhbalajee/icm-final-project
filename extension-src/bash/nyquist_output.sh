4c42d6a13c4f9ede1a00c37027ddf7e125a63cbb
U1ltra <118020229@link.cuhk.edu.cn>
2024-03-25 16:59:52 -0400

    feat: add nyquist support

---
+++ extension-src/bash/nyquist_output.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+clear
+
+PIPE_FILE=/tmp/control_editor_pipe
+
+# Define cleanup procedure
+cleanup() {
+    tmux kill-session
+    exit
+}
+
+# Set trap to call cleanup function when SIGINT (Ctrl+C), SIGTERM or EXIT signal is received
+trap 'cleanup' EXIT INT TERM HUP ERR
+
+# Read input from the named pipe
+if [[ -p "$PIPE_FILE" ]]; then
+    # Continuously display the contents of the log file
+    while [[ -p "$PIPE_FILE" ]]; do
+
+        echo -n "SAL> "
+
+        if read -t 1 -r input < "$PIPE_FILE"; then
+            # Print the input
+            echo "$input"
+            
+            if [[ "$input" == "exit" ]]; then
+                break
+            fi
+
+            # Execute the command with process_nyquist_input.sh
+            ./process_nyquist_input.sh -c "$input"
+        fi
+    
+    done
+else
+    echo "Named pipe does not exist."
+fi
+
+trap - INT TERM EXIT HUP ERR
+
+exit
